46a1c591bacbcc1b606e61a5182cc86b20decadd
Maribelens <maribelensosa@gmail.com>
2026-07-13 16:03:08 -0300

    Add(UI) Options panel in MainMenu

--- Assets/Scripts/UI/UIChangeScene.cs
+++ Assets/Scripts/UI/UIChangeScene.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class UIChangeScene : MonoBehaviour
 {
     [Header("Menu Panel")]
     [SerializeField] private CanvasGroup menuCanvasGroup;
     [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
     [SerializeField] private Button creditsButton;
     [SerializeField] private Button exitButton;
 
@@ -14,16 +16,23 @@ public class UIChangeScene : MonoBehaviour
     [SerializeField] private CanvasGroup creditsCanvasGroup;
     [SerializeField] private Button creditsBackButton;
 
+    [Header("Options Panel")]
+    [SerializeField] private CanvasGroup optionsCanvasGroup;
+    [SerializeField] private Button optionsBackButton;
+
     public void Awake()
     {
         AddButtonsListeners();
         SetStateCanvasGroup(menuCanvasGroup, true);
+        SetStateCanvasGroup(optionsCanvasGroup, false);
         SetStateCanvasGroup(creditsCanvasGroup, false);
     }
 
     private void AddButtonsListeners()
     {
         playButton.onClick.AddListener(OnPlayClicked);
+        optionsButton.onClick.AddListener(OnOptionsClicked);
+        optionsBackButton.onClick.AddListener(OnOptionsBackClicked);
         creditsButton.onClick.AddListener(OnCreditsClicked);
         creditsBackButton.onClick.AddListener(OnCreditsBackClicked);
         exitButton.onClick.AddListener(OnExitClicked);
@@ -35,11 +44,30 @@ public class UIChangeScene : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    private void OnOptionsClicked()
+    {
+        SetStateCanvasGroup(menuCanvasGroup, false);
+        SetStateCanvasGroup(optionsCanvasGroup, true);
+    }
+
+    private void OnOptionsBackClicked()
+    {
+        SetStateCanvasGroup(menuCanvasGroup, true);
+        SetStateCanvasGroup(optionsCanvasGroup, false);
+    }
+
     private void OnCreditsClicked()
     {
         SetStateCanvasGroup(menuCanvasGroup, false);
         SetStateCanvasGroup(creditsCanvasGroup, true);
-       
+
+    }
+
+    private void OnCreditsBackClicked()
+    {
+        SetStateCanvasGroup(menuCanvasGroup, true);
+        SetStateCanvasGroup(creditsCanvasGroup, false);
+
     }
 
     private void OnExitClicked()
@@ -58,13 +86,6 @@ public class UIChangeScene : MonoBehaviour
         canvasGroup.alpha = state ? 1 : 0;
         canvasGroup.interactable = state;
         canvasGroup.blocksRaycasts = state;
-    }
-
-    private void OnCreditsBackClicked()
-    {
-        SetStateCanvasGroup(menuCanvasGroup, true);
-        SetStateCanvasGroup(creditsCanvasGroup, false);
-       
     }
 
     public void OnDestroy()
